a09e193289f0a9de4b4725ebd2dbc677219395f7
lolstgold <124512244+lolstgold@users.noreply.github.com>
2025-09-04 14:39:06 +0100

    Update attributesConvert.lua

--- attributesConvert.lua
+++ attributesConvert.lua
@@ -8,21 +8,31 @@ local newvals = {
 	["CFrame"] = "CFrameValue"
 }
 
-function Attributify(v3)
-	for _,v in pairs(v3:GetDescendants())  do
-		for q,v2 in v:GetAttributes() do
-			if not v:FindFirstChild("__ATTRIBUTES") then
-				Instance.new("Model",v).Name = "__ATTRIBUTES"
-			end
-			local new = Instance.new(newvals[typeof(v2)],v.__ATTRIBUTES)
-			new.Name = q
-			if typeof(v2) == "EnumItem" then
-				new.Value = v2.Name
-			else
-				new.Value = v2
-			end
+function attrib(v)
+	for q,v2 in v:GetAttributes() do
+		if not v:FindFirstChild("__ATTRIBUTES") then
+			Instance.new("Model",v).Name = "__ATTRIBUTES"
+		end
+		local new = Instance.new(newvals[typeof(v2)],v.__ATTRIBUTES)
+		new.Name = q
+		if typeof(v2) == "EnumItem" then
+			new.Value = v2.Name
+		else
+			new.Value = v2
 		end
 	end
 end
+function Attributify(v3)
+	for _,v in pairs(v3:GetDescendants())  do
+		attrib(v)
+	end
+	attrib(v3)
+end
 
 Attributify(workspace)
+Attributify(game.ServerStorage)
+Attributify(game.Lighting)
+Attributify(game.ServerScriptService)
+Attributify(game.ReplicatedStorage)
+Attributify(game.StarterGui)
+Attributify(game.Players)
